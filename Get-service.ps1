9e086443b1eea715d43790858531a038c4102dec
sudheer <sudheer.bethina9@gmail.com>
2019-09-27 18:22:10 +0530

    first commit

---
+++ Get-service.ps1
@@ -0,0 +1,11 @@
+﻿$Services = Get-Service
+
+$Services_output = $Services | ForEach-Object {
+[PSCustomObject]@{
+"ServiceName" = $_.ServiceName
+"Name" = $_.Name
+"Status" = $_.Status
+"DisplayName" = $_.DisplayName
+}
+}
+$Services_output | Export-Csv  -Force -NoTypeInformation C:\ansible\services.csv
---
+++ Hotfixes.ps1
@@ -0,0 +1,14 @@
+﻿$Hotfixes = Get-HotFix 
+
+
+$HotFixes_output = $Hotfixes | ForEach-Object {
+[PSCustomObject]@{ 
+"ComputerName" = $_.PSComputerName
+"Description" = $_.Description
+"HotFixID" = $_.HotFixID
+"InstalledDate" = $_.InstalledOn
+"InstalledBy" = $_.InstalledBy
+ }
+}
+
+$HotFixes_output | Export-Csv  -Force -NoTypeInformation C:\ansible\Hotfixes.csv
